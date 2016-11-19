8d524b6921b7ffd071e590142c6f6763aa7c2525
Brian Lee <brianjleeofcl@gmail.com>
2016-11-18 17:13:49 -0800

    Add check quantity

--- js/order.js
+++ js/order.js
@@ -5,4 +5,23 @@
   $(document).ready(() => {
     $('select').material_select();
   });
+
+  const checkQuantity = function($target) {
+    const $input = $target.parents('.card').find('input[type="number"]')
+    if ($input.val() === '') {
+      console.log('Empty');
+      return
+    }
+    console.log('something');
+    return $input.val()
+  }
+
+  // add to cart links
+  $('.add-to-cart').click(() => {
+    if ($(event.target).hasClass('need-quantity')) {
+      const quantity = checkQuantity($(event.target));
+      console.log(quantity);
+    }
+
+  })
 })();
