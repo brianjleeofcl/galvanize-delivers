cd4218957b3acf91aa451f8ffbcb7f53d6f24f08
Brian Lee <brianjleeofcl@gmail.com>
2016-11-19 12:22:57 -0800

    Add cart, lint

--- js/order.js
+++ js/order.js
@@ -7,21 +7,52 @@
   });
 
   const checkQuantity = function($target) {
-    const $input = $target.parents('.card').find('input[type="number"]')
+    const $input = $target.parents('.card').find('input[type="number"]');
+
     if ($input.val() === '') {
-      console.log('Empty');
-      return
+      Materialize.toast('Please enter a quantity.', 3000, 'center-screen');
+
+      return '';
     }
-    console.log('something');
-    return $input.val()
-  }
+
+    return $input.val();
+  };
+
+  const calculatePrice = function(item, quantity) {
+    return 4;
+  };
+
+  const addToCart = function(item, quantity) {
+    const $row = $('<tr>');
+    const $item = $('<td>');
+    const $quantity = $('<td>');
+    const $price = $('<td>');
+    const price = calculatePrice(item, quantity);
+
+    $item.text(item);
+    $quantity.text(quantity);
+    $price.text(price);
+    $row.append($item).append($quantity).append($price);
+
+    return $row;
+  };
 
   // add to cart links
   $('.add-to-cart').click(() => {
+    const $cart = $('#cart');
+    const item = 'a';
+    let quantity;
+
     if ($(event.target).hasClass('need-quantity')) {
-      const quantity = checkQuantity($(event.target));
-      console.log(quantity);
+      quantity = checkQuantity($(event.target));
+    } else {
+      quantity = 1;
     }
 
-  })
+    if (quantity === '') {
+      return;
+    }
+
+    $cart.append(addToCart(item, quantity));
+  });
 })();
