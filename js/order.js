a67d37adb56fa623999426cdfac8fb8e78f84532
Brian Lee <brianjleeofcl@gmail.com>
2016-11-19 18:24:29 -0800

    Add totals, lint

--- js/order.js
+++ js/order.js
@@ -6,9 +6,7 @@
     $('select').material_select();
   });
 
-  const checkQuantity = function($target) {
-    const $input = $target.parents('.card').find('input[type="number"]');
-
+  const checkQuantity = function($input) {
     if ($input.val() === '') {
       Materialize.toast('Please enter a quantity.', 3000, 'center-screen');
 
@@ -18,33 +16,83 @@
     return $input.val();
   };
 
-  const calculatePrice = function(item, quantity) {
-    return 4;
+  const retrievePrice = function(item) {
+    switch (item) {
+      case 'item1':
+        return 49.99;
+      case 'item2':
+        return 12.99;
+      case 'item3':
+        return 9.99;
+      case 'item4':
+        return 7.99;
+      default:
+        return;
+    }
   };
 
-  const addToCart = function(item, quantity) {
+  const calculatePrice = function($card, quantity) {
+    const item = $card.attr('id');
+    const itemPrice = retrievePrice(item);
+
+    return itemPrice * quantity;
+  };
+
+  const addToCart = function($card, quantity) {
     const $row = $('<tr>');
     const $item = $('<td>');
     const $quantity = $('<td>');
     const $price = $('<td>');
-    const price = calculatePrice(item, quantity);
+    const price = calculatePrice($card, quantity);
 
-    $item.text(item);
-    $quantity.text(quantity);
-    $price.text(price);
+    if ($(event.target).hasClass('need-quantity')) {
+      const $quantityField = $('<input>').attr('type', 'number');
+
+      $quantityField.val(quantity);
+      $quantity.append($quantityField);
+    } else {
+      $quantity.text(quantity);
+    }
+
+    $item.text($card.find('h5').text());
+    $price.text(`$${price}`).addClass('right-align price');
     $row.append($item).append($quantity).append($price);
 
     return $row;
   };
 
+  const clearQuantityField = function($target) {
+    $target.val('');
+
+    return;
+  };
+
+  const retrieveTaxRate = function() {
+    return 0.098;
+  };
+
+  const updateTotal = function() {
+    const $prices = $('.price').text();
+    const arr = $prices.match(/([^$]+)/g).map(parseFloat);
+    const subtotal = arr.reduce((a, b) => a + b);
+    const taxRate = retrieveTaxRate();
+    const tax = subtotal * taxRate;
+    const total = subtotal + tax;
+
+    $('#subtotal').text(`$${subtotal.toFixed(2)}`);
+    $('#tax').text(`$${tax.toFixed(2)}`);
+    $('#total').text(`$${total.toFixed(2)}`);
+  };
+
   // add to cart links
   $('.add-to-cart').click(() => {
     const $cart = $('#cart');
-    const item = 'a';
+    const $card = $(event.target).parents('.card');
+    const $inputQuantity = $card.find('input[type="number"]');
     let quantity;
 
     if ($(event.target).hasClass('need-quantity')) {
-      quantity = checkQuantity($(event.target));
+      quantity = checkQuantity($inputQuantity);
     } else {
       quantity = 1;
     }
@@ -53,6 +101,8 @@
       return;
     }
 
-    $cart.append(addToCart(item, quantity));
+    $cart.append(addToCart($card, quantity));
+    clearQuantityField($inputQuantity);
+    updateTotal();
   });
 })();
